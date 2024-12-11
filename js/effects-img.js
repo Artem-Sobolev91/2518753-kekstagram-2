eb53eba5cb196a110c6110cf02a7e4177d8b6f10
Artem-Sobolev91 <167480489+Artem-Sobolev91@users.noreply.github.com>
2024-12-11 21:25:52 +0300

    добавляет модуль effects-img

---
+++ js/effects-img.js
@@ -0,0 +1,115 @@
+import {CLASS_NAME_HIDDEN} from'./util.js';
+import {imgUploadPreview} from'./load-form.js';
+
+const effectsPreview = document.querySelectorAll('.effects__radio');
+const effectsContainer = document.querySelector('.effect-level__slider');
+const effectLevelValue = document.querySelector('.effect-level__value');
+
+function defaultEffects () {
+  imgUploadPreview.className = '';
+  imgUploadPreview.style = '';
+  effectLevelValue.value = '';
+  effectsContainer.classList.add(CLASS_NAME_HIDDEN);
+
+}
+
+
+noUiSlider.create(effectsContainer,{
+  range: {
+    min: 0,
+    max: 1
+
+  },
+  start: 1,
+  step: 0.1
+});
+effectsContainer.noUiSlider.on('update', (values) => {
+  effectLevelValue.value = values[0];
+});
+
+effectsPreview.forEach((effectChecked) => {
+  effectChecked.addEventListener('change',()=>{
+    const selectEffect = effectChecked.value;
+    imgUploadPreview.className = '';
+    imgUploadPreview.classList.add(`effect__preview--${selectEffect}`);
+
+    switch(selectEffect){
+      case 'chrome':
+        effectsContainer.noUiSlider.updateOptions({
+          range:{
+            min:0,
+            max:1
+          },
+          step:0.1,
+          start:0
+
+        });
+        effectsContainer.noUiSlider.on('update',(evt)=>{
+          imgUploadPreview.style.filter = `grayscale(${evt[0]})`;
+        });
+        break;
+      case 'marvin':
+        effectsContainer.noUiSlider.updateOptions({
+          range:{
+            min:0,
+            max:100
+          },
+          step:1,
+          start:0
+
+        });
+        effectsContainer.noUiSlider.on('update',(evt)=>{
+          imgUploadPreview.style.filter = `invert(${evt[0]}%)`;
+        });
+        break;
+
+      case'sepia':
+        effectsContainer.noUiSlider.updateOptions({
+          range:{
+            min:0,
+            max:1
+          },
+          step:0.1,
+          start:0
+
+        });
+        effectsContainer.noUiSlider.on('update',(evt)=>{
+          imgUploadPreview.style.filter = `sepia(${evt[0]})`;
+        });
+        break;
+
+      case'phobos':
+        effectsContainer.noUiSlider.updateOptions({
+          range:{
+            min:0,
+            max:3
+          },
+          step:0.1,
+          start:0
+
+        });
+        effectsContainer.noUiSlider.on('update',(evt)=>{
+          imgUploadPreview.style.filter = `blur(${evt[0]}px)`;
+        });
+        break;
+
+      case'heat':
+        effectsContainer.noUiSlider.updateOptions({
+          range:{
+            min:1,
+            max:3
+          },
+          step:0.1,
+          start:0
+
+        });
+        effectsContainer.noUiSlider.on('update',(evt)=>{
+          imgUploadPreview.style.filter = `brightness(${evt[0]})`;
+        });
+        break;
+
+      default:
+        defaultEffects();
+    }
+  });
+});
